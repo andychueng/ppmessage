2f58ec4ec2595c6b12422ff453fd1dc056d482c2
Guijin Ding <dingguijin@gmail.com>
2017-01-22 19:14:56 +0800

    update ppconfig

--- ppmessage/ppkefu/src/js/controllers/main-with-logo.js
+++ ppmessage/ppkefu/src/js/controllers/main-with-logo.js
@@ -2,14 +2,12 @@ ppmessageModule.controller("MainWithLogoCtrl", [
     "$scope",
     "yvSys",
     "yvNav",
-    "yvLog",
     "yvMain",
     "yvUser",
-    "yvFile",
     "yvLogin",
     "yvUpdater",
     "yvConstants",
-function ($scope, yvSys, yvNav, yvLog, yvMain, yvUser, yvFile, yvLogin, yvUpdater, yvConstants) {
+function ($scope, yvSys, yvNav, yvMain, yvUser, yvLogin, yvUpdater, yvConstants) {
 
     function nav_login(user) {
         yvNav.clear(function () {
@@ -23,18 +21,15 @@ function ($scope, yvSys, yvNav, yvLog, yvMain, yvUser, yvFile, yvLogin, yvUpdate
         });
     }
 
-    function init_db() {
-        yvMain.init_yvdb(function (user) {
-            yvUpdater.check_update();
-            nav_login(user);
-        });
-    }
-
     function init() {
-        if (yvSys.in_mobile_app()) {
-            yvFile.init();
-        }
-        init_db();
+        console.log("0000000000000000");
+        yvMain.init_yvdb(function (user) {
+            console.log("0000000000000000111111");
+            yvUpdater.check_update();
+            console.log("00000000000000003333333");
+            nav_login(user);
+            console.log("0000000000000000222222");
+        });
     }
 
     init();
